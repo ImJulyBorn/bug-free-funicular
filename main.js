b8061eaed04eac3db57bfee830c45cf162f3d90d
ImJulyBorn <106574769+ImJulyBorn@users.noreply.github.com>
2022-09-21 16:33:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+status1 = "";
+function preload(){}
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(400, 400);
+    video.hide();
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status : detecting objects";
+}
+function modelLoaded(){
+    console.log("model loaded");
+    status1 = true;
+}
+function draw(){
+    image(video, 0, 0, 400, 400)
+}
